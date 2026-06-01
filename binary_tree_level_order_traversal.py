630e626264e0513aa6fc0ee826342848bf285feb
Mythili Kotaru <mythili@hashclicksolutions.com>
2026-06-01 15:46:19 -0400

    Add Binary Tree Level Order Traversal solution

---
+++ binary_tree_level_order_traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+            
+        queue = deque([root])
+        res = []
+
+        while queue:
+            lis = []
+            for _ in range(len(queue)):
+            
+                elem = queue.popleft()
+                lis.append(elem.val)
+
+                if elem.left:
+                    queue.append(elem.left)
+                if elem.right:
+                    queue.append(elem.right)
+
+            res.append(lis)
+        return res
